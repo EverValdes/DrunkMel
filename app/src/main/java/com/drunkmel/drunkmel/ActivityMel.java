d44002fe8393ec2f5932ee9a54cf577c85e81df7
evaldes <evervaldes621@gmail.com>
2017-01-31 14:28:58 -0300

    -Overring hardback button behavior to perform nothing by default.

--- app/src/main/java/com/drunkmel/drunkmel/ActivityMel.java
+++ app/src/main/java/com/drunkmel/drunkmel/ActivityMel.java
@@ -9,4 +9,10 @@ public class ActivityMel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    @Override
+    public void onBackPressed() {
+        //We do not want to perform anything when hard back button is pressed.
+    }
+
 }
