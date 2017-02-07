a5f43d3fd9139834b09c3cd9a63ce2dff8153b36
evaldes <evervaldes621@gmail.com>
2017-02-07 18:06:02 -0300

    -Adding next button disabling logic

--- app/src/main/java/com/drunkmel/drunkmel/PlayerActivity.java
+++ app/src/main/java/com/drunkmel/drunkmel/PlayerActivity.java
@@ -6,19 +6,22 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class PlayerActivity extends ActivityMel {
 
     private TextView playerLabel;
     private Typeface custom_font;
-    private Button addPlayer, next;
-    private LinearLayout linearLayout;
+    private Button addPlayerButton, nextButton;
+    private LinearLayout playerItem;
     private SharedPreferences sp_PlayerList, sp_PlayerScore, sp_PlayerTurn;
     private Context context;
 
@@ -31,6 +34,7 @@ public class PlayerActivity extends ActivityMel {
         createSharedPreferences();
 
         loadUI();
+        setDefaultPlayers(1);
     }
 
     private void createSharedPreferences() {
@@ -62,32 +66,52 @@ public class PlayerActivity extends ActivityMel {
         playerLabel = (TextView) findViewById(R.id.playerLabel);
         custom_font = Typeface.createFromAsset(getAssets(),  "fonts/Frijole-Regular.ttf");
         playerLabel.setTypeface(custom_font);
-        addPlayer = (Button) findViewById(R.id.addPlayer);
-        next = (Button) findViewById(R.id.next);
-        linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        addPlayerButton = (Button) findViewById(R.id.addPlayer);
+        nextButton = (Button) findViewById(R.id.next);
+        playerItem = (LinearLayout) findViewById(R.id.playerItem);
 
         setListeners(context);
+        enableButton(nextButton, false);
     }
 
     private void setListeners(final Context context) {
         //Listeners
-        addPlayer.setOnClickListener(new View.OnClickListener() {
+        addPlayerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //Creating a new edit text for a new player
-                EditText playerItem = new EditText(context);
-                playerItem.setLayoutParams(new LinearLayout.LayoutParams(
+                EditText playerName = new EditText(context);
+
+                playerName.addTextChangedListener(new TextWatcher() {
+
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        configureNextButton();
+                    }
+                });
+
+                playerName.setLayoutParams(new LinearLayout.LayoutParams(
                         LinearLayout.LayoutParams.MATCH_PARENT,
                         LinearLayout.LayoutParams.WRAP_CONTENT));
-                playerItem.setFocusableInTouchMode(true);
-                playerItem.requestFocus();
-                playerItem.setHint(R.string.newPlayerHint);
-                playerItem.setHintTextColor(Color.WHITE);
-                playerItem.setTextColor(Color.WHITE);
-                linearLayout.addView(playerItem);
+                playerName.setFocusableInTouchMode(true);
+                playerName.requestFocus();
+                playerName.setHint(R.string.newPlayerHint);
+                playerName.setHintTextColor(Color.WHITE);
+                playerName.setTextColor(Color.WHITE);
+                playerItem.addView(playerName);
             }
         });
 
-        next.setOnClickListener(new View.OnClickListener() {
+        nextButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //Get data from previous intent and create a new one depending the game mode
                 String gameMode = getIntent().getExtras().getString("GAME_MODE");
@@ -101,8 +125,8 @@ public class PlayerActivity extends ActivityMel {
                 }
 
                 //Get the name of all the players checking the children of the container
-                for(index=0; index<linearLayout.getChildCount(); ++index) {
-                    EditText nextChild = (EditText) linearLayout.getChildAt(index);
+                for(index=0; index< playerItem.getChildCount(); ++index) {
+                    EditText nextChild = (EditText) playerItem.getChildAt(index);
 
                     setPlayerList(Integer.toString(index), nextChild.getText().toString());
                     setPlayerScore(Integer.toString(index));
@@ -120,6 +144,40 @@ public class PlayerActivity extends ActivityMel {
                 finish();
             }
         });
+    }
+
+    private void setDefaultPlayers(int numberOfPlayers){
+        for (int i = 1; i<= numberOfPlayers; i++){
+            addPlayerButton.performClick();
+        }
+    }
+
+    private void configureNextButton() {
+        //All the fields for players into this activity
+        int amountOfPlayers = playerItem.getChildCount();
+        //If there are least than 2 players, don't enable the nextButton button
+        if (amountOfPlayers < 2) {
+            enableButton(nextButton, false);
+        } else {
+            for (int i = 0; i < amountOfPlayers; i++) {
+                EditText player = (EditText) playerItem.getChildAt(i);
+                if (StringUtils.isEmpty(player.getText())) {
+                    enableButton(nextButton, false);
+                    return;
+                } else {
+                    enableButton(nextButton, true);
+                }
+            }
+        }
+    }
+
+    private void enableButton(Button button, boolean enable) {
+        button.setEnabled(enable);
+        if (enable) {
+            button.setAlpha(1F);
+        } else {
+            button.setAlpha(0.7F);
+        }
     }
 
     private void setPlayerList(String id, String player) {
